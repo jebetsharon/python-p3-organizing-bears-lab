dd62b5f7f479a9e1a463ebe68a53ee546ced724c
Sharon kosgei <jebetsharon089@gmail.com>
2025-05-23 13:28:23 +0300

    complete organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 6, "F", "blue", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 40, "F", "white", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 23, "M", "blank", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 1, "M", "white", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 12, "F", "violet", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 4, "M", "blue", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 5, "M", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 5, "F", "black", "aggressive", 0);
